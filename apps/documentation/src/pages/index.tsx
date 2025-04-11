6b63b266365f16484412febab4dd64ac791d2138
Aidan Kinzett <aidan@kinzett.io>
2025-04-11 14:39:26 +1000

    feat: add project structure overview page

--- apps/documentation/src/pages/index.tsx
+++ apps/documentation/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/project-structure"
+            to="/docs/repo-configuration/project-structure"
           >
             Project Structure
           </Link>
