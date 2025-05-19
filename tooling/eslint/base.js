facb3a6175599259f5e21e02e96b52df07cdbc89
Aidan Kinzett <aidan@kinzett.io>
2025-05-20 09:34:14 +1000

    feat: add eslint warning for todo comments

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -83,6 +83,7 @@ export default tseslint.config(
           rule: "kebab-case",
         },
       ],
+      "no-warning-comments": "warn",
     },
   },
   // apply to all files, but not to pages or layouts
